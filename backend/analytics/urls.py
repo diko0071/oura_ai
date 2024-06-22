99480179914561d625492ad5cfecd3660b52ae8a
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-22 23:51:11 +0500

    api for oura data for main metrics

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('oura/daily_readiness/', OuraData.as_view(), name='oura-daily-readiness'),
+    path('oura/', oura_data, name='oura-data'),
 ]
--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -4,26 +4,175 @@ import requests
 import os 
 from dotenv import load_dotenv
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
 
 load_dotenv()
 
-class OuraData(APIView):
-    oura_token = os.getenv("OURA_TOKEN")
-    headers = { 
-        'Authorization': 'Bearer ' + oura_token 
-    }
-    start_date = '2023-02-01'
-    end_date = '2024-06-01'
 
-    def get(self, request):
-        url = 'https://api.ouraring.com/v2/usercollection/daily_readiness' 
-        headers = { 
-            'Authorization': 'Bearer ' + self.oura_token 
-        }
-        params={ 
-            'start_date': self.start_date, 
-            'end_date': self.end_date 
-        }
-        response = requests.request('GET', url, headers=headers, params=params)
+def get_sleep_data(start_date, end_date):
+
+    api = 'daily_sleep'
+
+    url = f'https://api.ouraring.com/v2/usercollection/{api}'
+    params={ 
+        'start_date': start_date, 
+        'end_date': end_date 
+    }
+    headers = { 
+        'Authorization': f'Bearer {os.getenv("OURA_TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    data = response.json()['data']
+
+    rows = []
+
+    for item in data: 
+        row = item.copy()
+
+        if 'contributors' in item:
+            row.update(item['contributors'])
+            del row['contributors']
         
-        return Response(response.json())
+        rows.append(row)
+    
+    return rows
+
+def get_readiness_data(start_date, end_date):
+
+    api = 'daily_readiness'
+
+    url = f'https://api.ouraring.com/v2/usercollection/{api}'
+    params={ 
+        'start_date': start_date, 
+        'end_date': end_date 
+    }
+    headers = { 
+        'Authorization': f'Bearer {os.getenv("OURA_TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    data = response.json()['data']
+
+    rows = []
+
+    for item in data: 
+        row = item.copy()
+
+        if 'contributors' in item:
+            row.update(item['contributors'])
+            del row['contributors']
+        
+        rows.append(row)
+    
+    return rows
+
+
+def get_stress_data(start_date, end_date):
+
+    api = 'daily_stress'
+
+    url = f'https://api.ouraring.com/v2/usercollection/{api}'
+    params={ 
+        'start_date': start_date, 
+        'end_date': end_date 
+    }
+    headers = { 
+        'Authorization': f'Bearer {os.getenv("OURA_TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    data = response.json()['data']
+
+    rows = []
+
+    for item in data: 
+        row = item.copy()
+
+        if 'contributors' in item:
+            row.update(item['contributors'])
+            del row['contributors']
+        
+        rows.append(row)
+    
+    return rows
+
+def get_activity_data(start_date, end_date):
+
+    api = 'daily_activity'
+
+    url = f'https://api.ouraring.com/v2/usercollection/{api}'
+    params={ 
+        'start_date': start_date, 
+        'end_date': end_date 
+    }
+    headers = { 
+        'Authorization': f'Bearer {os.getenv("OURA_TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    data = response.json()['data']
+
+    rows = []
+
+    for item in data: 
+        row = item.copy()
+
+        if 'contributors' in item:
+            row.update(item['contributors'])
+            del row['contributors']
+        
+        rows.append(row)
+    
+    return rows
+
+
+def get_heart_data(start_date, end_date):
+
+    api = 'heartrate'
+
+    url = f'https://api.ouraring.com/v2/usercollection/{api}'
+    params={ 
+        'start_date': start_date, 
+        'end_date': end_date 
+    }
+    headers = { 
+        'Authorization': f'Bearer {os.getenv("OURA_TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    data = response.json()['data']
+
+    rows = []
+
+    for item in data: 
+        row = item.copy()
+
+        if 'contributors' in item:
+            row.update(item['contributors'])
+            del row['contributors']
+        
+        rows.append(row)
+    
+    return rows
+
+@api_view(['GET'])
+def oura_data(request):
+    start_date = '2022-01-01'
+    end_date = '2024-12-01'
+
+    sleep_data = get_sleep_data(start_date, end_date)
+    readiness_data = get_readiness_data(start_date, end_date)
+    stress_data = get_stress_data(start_date, end_date)
+    activity_data = get_activity_data(start_date, end_date)
+    heart_data = get_heart_data(start_date, end_date)
+
+
+    return Response({
+        'sleep_data': sleep_data,
+        'readiness_data': readiness_data,
+        'stress_data': stress_data,
+        'activity_data': activity_data,
+        'heart_data': heart_data
+    })
